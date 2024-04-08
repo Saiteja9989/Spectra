2a7f7c13c0b94c1958a7ed3f7838b43c2bbc1108
Saiteja9989 <kasojusaiteja10@gmail.com>
2024-04-08 23:34:05 +0530

    lateral entry

--- Frontend/src/components/Clubs.jsx
+++ Frontend/src/components/Clubs.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Card, Typography } from 'antd';
-import { ClusterOutlined } from '@ant-design/icons';
-import Swal from 'sweetalert2'; // Import SweetAlert
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { Card, Typography } from 'antd';
+// import { ClusterOutlined } from '@ant-design/icons';
+// import Swal from 'sweetalert2'; // Import SweetAlert
 
-const { Meta } = Card;
-const { Title } = Typography;
+// const { Meta } = Card;
+// const { Title } = Typography;
 
-const ProfilePage = () => {
+// const ProfilePage = () => {
 
-  // Function to handle click on "Clubs" option
-  const handleClubsClick = () => {
-    Swal.fire({
-      icon: 'info',
-      title: 'Club Details',
-      text: 'Club details will be updated soon.',
-    });
-  };
+//   // Function to handle click on "Clubs" option
+//   const handleClubsClick = () => {
+//     Swal.fire({
+//       icon: 'info',
+//       title: 'Club Details',
+//       text: 'Club details will be updated soon.',
+//     });
+//   };
 
-  return (
-    <div>
-      <Title level={4}>Options</Title>
-      {/* Add onClick event to trigger the handleClubsClick function */}
-      <Link to="/clubs" onClick={handleClubsClick}>
-        <Card hoverable className="option-card">
-          <Meta title="Clubs" avatar={<ClusterOutlined />} />
-        </Card>
-      </Link>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <Title level={4}>Options</Title>
+//       {/* Add onClick event to trigger the handleClubsClick function */}
+//       <Link to="/clubs" onClick={handleClubsClick}>
+//         <Card hoverable className="option-card">
+//           <Meta title="Clubs" avatar={<ClusterOutlined />} />
+//         </Card>
+//       </Link>
+//     </div>
+//   );
+// };
 
-export default ProfilePage;
+// export default ProfilePage;
