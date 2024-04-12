c6f00724034c162456515d0b4fa56f0c919375aa
Saiteja9989 <kasojusaiteja10@gmail.com>
2024-04-12 23:40:35 +0530

    ..

--- Backend/server.js
+++ Backend/server.js
@@ -38,14 +38,14 @@ app.use(cors({
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('products api running new deploy');
+  res.send('backend uploaded..!');
 });
 
-// app.use(cors({
-//   origin: 'http://localhost:5173',
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 // Enable CORS for all routes
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Update to match the domain you will make the request from
