84bc09ff4875fc6fedf8c58683b8f39cde251836
Saiteja9989 <kasojusaiteja10@gmail.com>
2024-04-12 23:52:49 +0530

    vv

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ const externalexam= require('./routes/externalres')
 const app = express();
 const PORT = process.env.PORT || 5000  ;
 app.use(cors({
-  origin: '*', // Change this to your React app's URL
+  origin: 'https://spectra-beta.vercel.app/', // Change this to your React app's URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Enable credentials (if required)
 }));
@@ -41,11 +41,11 @@ app.get('/', (req, res) => {
   res.send('backend uploaded..');
 });
 
-// app.use(cors({
-//   origin: 'http://localhost:5173',
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 // Enable CORS for all routes
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Update to match the domain you will make the request from
