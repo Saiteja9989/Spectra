7ed217f0f4bd5397d3e52862add37c66f77fbc1a
Saiteja9989 <kasojusaiteja10@gmail.com>
2024-06-15 12:38:55 +0530

    ..

--- Frontend/src/components/dashboard.jsx
+++ Frontend/src/components/dashboard.jsx
@@ -57,7 +57,7 @@ const ProfilePage = ({ netraID }) => {
       Swal.fire({
         icon: 'warning',
         title: 'KINDLY WAIT',
-        text: 'CURRENTLY NETRA SERVER IS DOWN',
+        text: 'Under maintanance',
         confirmButtonText: 'OK'
       });
     }
