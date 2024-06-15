102f2c87284eb984de2c14af4eb6a63766ad5019
Saiteja9989 <kasojusaiteja10@gmail.com>
2024-06-15 14:47:21 +0530

    ,,..,,

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -21,30 +20,23 @@ const Getsubjects =require('./routes/getSemSubjects')
 const app = express();
 const PORT = process.env.PORT || 5000  ;
 app.use(cors({
-  origin: 'https://spectra-beta.vercel.app', // Change this to your React app's URL
+  origin: '*', // Change this to your React app's URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Enable credentials (if required)
 }));
 
 // Allow requests only from specific origins
 const corsOptions = {
-  origin: 'https://spectra-beta.vercel.app',
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
 
 app.use(cors({
-  origin: 'https://spectra-beta.vercel.app'
+  origin: '*'
 }));
-// const validateOrigin = (req, res, next) => {
-//   if (req.headers.origin !== 'https://spectra-beta.vercel.app') {
-//       return res.status(403).json({ error: 'Unauthorized request' });
-//   }
-//   next();
-// };
 
-// app.use(validateOrigin);
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -86,7 +78,7 @@ app.use('/api',Getsubjects)
 
 
 // DB Config
-// const db = process.env.CONNECTION
+const db = process.env.CONNECTION
 
 // app.post('/search', async (req, res) => {
 //   const searchInput = req.body.searchInput;
