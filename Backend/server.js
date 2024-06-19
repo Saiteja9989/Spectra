027fa3cd070c28d4e6ed69a895341010dfb4ec5a
Abhi-Gx <abhigxtheupm@gmail.com>
2024-06-20 02:31:42 +0530

    deployment problem take 4

--- Backend/server.js
+++ Backend/server.js
@@ -19,37 +19,22 @@ const Fetchqr = require('./routes/fetchqr')
 const Getsubjects =require('./routes/getSemSubjects')
 const app = express();
 const PORT = process.env.PORT || 5000  ;
-// app.use(cors({
-//   origin: 'https://spectra-beta.vercel.app', // Change this to your React app's URL
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true, // Enable credentials (if required)
-// }));
-
-// // Allow requests only from specific origins
-// const corsOptions = {
-//   origin: 'https://spectra-beta.vercel.app',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
-
-// app.use(cors(corsOptions));
-
-// app.use(cors({
-//   origin: 'https://spectra-beta.vercel.app'
-// }));
-// const validateOrigin = (req, res, next) => {
-//   if (req.headers.origin !== 'https://spectra-beta.vercel.app') {
-//       return res.status(403).json({ error: 'Unauthorized request' });
-//   }
-//   next();
-// };
-
-// app.use(validateOrigin);
-
-
 app.use(cors({
   origin: '*', // Change this to your React app's URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Enable credentials (if required)
+}));
+
+// Allow requests only from specific origins
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+app.use(cors({
+  origin: '*'
 }));
 
 app.use(express.json());
@@ -64,14 +49,14 @@ app.get('/', (req, res) => {
 //   credentials: true,
 // }));
 // Enable CORS for all routes
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*'); // Update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-// // Bodyparser Middleware
-// app.use(bodyParser.json());
+// Bodyparser Middleware
+app.use(bodyParser.json());
 
 app.use('/api', search)
 app.use('/api', netraid)
