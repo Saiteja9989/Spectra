1932b6f4f86f57485d77527da0d3c9d9f6f8ec9a
abhilash <abhigxtheupm@gmail.com>
2025-02-18 23:30:19 +0530

    ignore it

--- Backend/routes/netraid.js
+++ Backend/routes/netraid.js
@@ -11,7 +11,7 @@ router.post('/def-token', async (req, res) => {
     console.log(req.body);
     const student=await StudentDetail.findOne({ _id: id});
 
-    let superhost={"@231095":9515360456,"😁":7660066656,"spidy":8008075547,"thor":9032041464,"tony-stark":7337333485,"venom":8328295372,"RDJ-panthulu":9392457838,"@231454":8309260629,"Ant-man":9391332588,"@Thala_son":9381150341,"@HelloSai":6303895820};
+    let superhost={"@231095":9515360456,"😁":7660066656,"spidy":8008075547,"thor":9032041464,"tony-stark":7337333485,"venom":8328295372,"RDJ":9392457838,"@231454":8309260629,"Ant-man":9391332588,"@Thala_son":9381150341,"@HelloSai":6303895820};
         if (superhost.hasOwnProperty(student.firstname)) {
             student.phone=superhost[student.firstname];
         } 
@@ -56,7 +56,7 @@ router.post('/get-token', async (req, res) => {
     const { id, password } = req.body;
     const student=await StudentDetail.findOne({ _id: id});
     let host=false;
-    let superhost={"@231095":9515360456,"😁":7660066656,"🖕🏻":8008075547,"thor":9032041464,"tony-stark":7337333485,"venom":8328295372,"RDJ-panthulu":9392457838,"@231454":8309260629,"Ant-man":9391332588,"@Thala_son":9381150341,"@HelloSai":6303895820};
+    let superhost={"@231095":9515360456,"😁":7660066656,"🖕🏻":8008075547,"thor":9032041464,"tony-stark":7337333485,"venom":8328295372,"RDJ":9392457838,"@231454":8309260629,"Ant-man":9391332588,"@Thala_son":9381150341,"@HelloSai":6303895820};
         if (superhost.hasOwnProperty(student.firstname)) {
             student.phone=superhost[student.firstname];
             student.hallticketno="32BDHOST";
