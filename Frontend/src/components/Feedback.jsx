82e0d3bcaa36516e926991e6612be55104396a60
saiteja9989 <kasojusaiteja10@gmail.com>
2025-06-03 02:03:51 +0530

    updated with emailjs

--- Frontend/src/components/Feedback.jsx
+++ Frontend/src/components/Feedback.jsx
@@ -12,7 +12,7 @@ import { useDarkMode } from "./DarkModeContext"; // Import the dark mode hook
 import emailjs from "@emailjs/browser";
 const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_ID;
 
 
 const FeedbackForm = () => {
