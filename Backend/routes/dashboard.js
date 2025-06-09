e8301450643dc0b32ebb8f39f70d77265a1bf60e
abhilash <abhigxtheupm@gmail.com>
2025-06-10 00:14:50 +0530

    stalin reporting sir

--- Backend/routes/dashboard.js
+++ Backend/routes/dashboard.js
@@ -68,7 +68,8 @@ router.post('/profile', async (req, res) => {
       "7981991406": "https://spectraserver-indol.vercel.app/images/ritesh.png",
       "9398532147": "https://spectraserver-indol.vercel.app/images/varshith.png",
       "8125611565": "https://spectraserver-indol.vercel.app/images/nithin.png",
-      "7993186148": "https://spectraserver-indol.vercel.app/images/bhai.png"
+      "7993186148": "https://spectraserver-indol.vercel.app/images/bhai.png",
+      "8074654538": "https://spectraserver-indol.vercel.app/images/darshin.png"
     };
 
     if (Object.keys(imGroup).includes(String(profileData.phone))) {
