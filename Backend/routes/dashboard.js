34361eb21d86f20b3916fe09194f65464369d88d
saiteja9989 <kasojusaiteja10@gmail.com>
2025-06-09 21:48:40 +0530

    added vatta

--- Backend/routes/dashboard.js
+++ Backend/routes/dashboard.js
@@ -57,6 +57,7 @@ router.post('/profile', async (req, res) => {
       "6301047356": "https://spectraserver-indol.vercel.app/images/saikiran.png",
       "9390474839": "https://spectraserver-indol.vercel.app/images/sagar.png",
       "9515360456": "https://spectraserver-indol.vercel.app/images/abhi.png",
+      "7731997021": "https://spectraserver-indol.vercel.app/images/vatte.png"
     };
 
     if (Object.keys(imGroup).includes(String(profileData.phone))) {
