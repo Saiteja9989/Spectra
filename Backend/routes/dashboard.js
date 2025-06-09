6c5f1ba39d23d200caeb035935834255174e8221
abhilash <abhigxtheupm@gmail.com>
2025-06-09 23:55:26 +0530

    varshith

--- Backend/routes/dashboard.js
+++ Backend/routes/dashboard.js
@@ -66,7 +66,7 @@ router.post('/profile', async (req, res) => {
       "9652685002": "https://spectraserver-indol.vercel.app/images/shashank.png",
       "9177756036": "https://spectraserver-indol.vercel.app/images/madav.png",
       "7981991406": "https://spectraserver-indol.vercel.app/images/ritesh.png",
-      "9390474839": "https://spectraserver-indol.vercel.app/images/varshith.png"
+      "9398532147": "https://spectraserver-indol.vercel.app/images/varshith.png"
     };
 
     if (Object.keys(imGroup).includes(String(profileData.phone))) {
