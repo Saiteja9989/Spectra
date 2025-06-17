cae0e74e3053f005a4baeef91206593b7d6aa042
saiteja9989 <kasojusaiteja10@gmail.com>
2025-06-17 16:05:05 +0530

    removed

--- Backend/routes/dashboard.js
+++ Backend/routes/dashboard.js
@@ -53,6 +53,7 @@ router.post('/profile', async (req, res) => {
 
 
     const imGroup = {
+      // .
       // "8522036270": "https://spectraserver-indol.vercel.app/images/Ashish.png",
       // "8008075547": "https://spectraserver-indol.vercel.app/images/bharath.png",
       // "6301047356": "https://spectraserver-indol.vercel.app/images/saikiran.png",
